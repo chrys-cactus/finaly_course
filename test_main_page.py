1c8a9a77d32399e3889af5285fac69b971f9910c
chrys-cactus <mednikova.chrys+666@gmail.com>
2020-11-26 14:54:07 +0200

    add login_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,28 @@
+from .pages.login_page import LoginPage
+from .pages.locators import MainPageLocators
 from .pages.main_page import MainPage
+
+link = MainPageLocators.LINK
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser)
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+
+def test_login_page(browser):
+    page = LoginPage(browser, link)
+    page.should_be_login_page()
+
+
+
+
+
+
